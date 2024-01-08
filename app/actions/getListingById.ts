679f5e96ee66983fef7e892293f7d4ac32ec30fb
Abdulbasit <habdulbasit099@gmail.com>
2024-01-08 20:11:35 +0200

    finished single listing preview

---
+++ app/actions/getListingById.ts
@@ -0,0 +1,33 @@
+import prisma from "@/app/libs/prismadb";
+
+interface Iparams {
+  listingId: string;
+}
+
+export default async function getListingById(params: Iparams) {
+  try {
+    const { listingId } = params;
+
+    const listing = await prisma.listing.findUnique({
+      where: {
+        id: listingId,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!listing) {
+      return null;
+    }
+
+    return {
+      ...listing,
+      user: {
+        ...listing.user,
+      },
+    };
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
